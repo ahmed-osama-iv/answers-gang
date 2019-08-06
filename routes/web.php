dcd20f28dadf5870698ff81bff3c7dc2a1179808
2019mohamed <48313693+2019mohamed@users.noreply.github.com>
2019-08-06 10:33:49 +0200

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,52 @@
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+Route::get('/All','QuestionController@index');
+Route::get('/qa',function (){
+   return view('ShowAndA');
+});
+Route::post('/store','QuestionController@store');
+
+Route::post('/store_','AnswerController@store');
+
+Route::get('/show/{id}','QuestionController@show');
+
+Route::get('/qp' , function (){
+    return view('QuestionPage');
+});
+
+
+
+
+
+
+
+Route::get('/show','ControllerUser@index');
+
+Route::get('/edit/{id}' , 'ControllerUser@edit');
+
+
+
+
+Route::post('/create','ControllerUser@create');
+
+Route::post('/update','ControllerUser@update');
+
+Route::get('/delete/{id}','ControllerUser@delete');
+
+
+Route::get('/index1','ControllerUser@index1');
+
+Route::get('/create_',function(){
+   return view('create');
+});
+
+
