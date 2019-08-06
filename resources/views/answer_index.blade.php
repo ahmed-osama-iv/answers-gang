c12ea56dd5c9f39a01fbd97f498dadacf5a2620e
ahmed <ahmed.osama.iv@gmail.com>
2019-08-07 00:40:24 +0200

    Just a forgotten @section :"p

--- resources/views/answer_index.blade.php
+++ resources/views/answer_index.blade.php
@@ -1,4 +1,10 @@
-@foreach($qs as $o)
-    <h3>{{$o->id}} {{$o->content}}</h3>
-    <a href="/answers/edit/{{$o->id}}" class="btn btn-primary">edit</a> <a href="/answers/delete/{{$o->id}}" class="btn btn-danger">delete</a>
-@endforeach
+@extends('layouts/master')
+
+@section('content')
+
+    @foreach($qs as $o)
+        <h3>{{$o->id}} {{$o->content}}</h3>
+        <a href="/answers/edit/{{$o->id}}" class="btn btn-primary">edit</a> <a href="/answers/delete/{{$o->id}}" class="btn btn-danger">delete</a>
+    @endforeach
+
+@endsection
